47dacffe5910af00bcd9ec4bd8fb71736dfe859c
kamalMakarim <kamal.makarim@gmail.com>
2023-10-06 11:21:33 +0700

    CS5-Selesai

--- src/kamalMakarimJBusRD/Algorithm.java
+++ src/kamalMakarimJBusRD/Algorithm.java
@@ -1,8 +1,7 @@
 
 package kamalMakarimJBusRD;
 
-import java.util.Iterator;
-import java.util.Arrays;
+import java.util.*;
 
 public class Algorithm {
 
@@ -39,5 +38,112 @@ public class Algorithm {
             }
         }
         return false;
+    }
+
+    public static <T> List<T> collect(T[] array, T value) {
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return collect(it, value);
+    }
+
+    public static <T> List<T> collect(Iterable<T> iterable, T value) {
+        final Iterator<T> it = iterable.iterator();
+        return collect(it, value);
+    }
+
+    public static <T> List<T>  collect(Iterator<T> iterator, T value) {
+        final Predicate<T> pred = value::equals;
+        return collect(iterator, pred);
+    }
+
+    public static <T> List<T>  collect(T[] array, Predicate<T> pred) {
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return collect(it, pred);
+    }
+
+    public static <T> List<T>  collect(Iterable<T> iterable, Predicate<T> pred) {
+        final Iterator<T> it = iterable.iterator();
+        return collect(it, pred);
+    }
+
+    public static <T> List<T>  collect(Iterator<T> iterator, Predicate<T> pred) {
+        List<T> result = new LinkedList<T>();
+        while (iterator.hasNext()) {
+            T current = iterator.next();
+            if (pred.predicate(current)) {
+                result.add(current);
+            }
+        }
+        return result;
+    }
+
+    public static <T> int count(T[] array, T value) {
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return count(it, value);
+    }
+
+    public static <T> int count(Iterable<T> iterable, T value) {
+        final Iterator<T> it = iterable.iterator();
+        return count(it, value);
+    }
+
+    public static <T> int count(Iterator<T> iterator, T value) {
+        final Predicate<T> pred = value::equals;
+        return count(iterator, pred);
+    }
+
+    public static <T> int count(T[] array, Predicate<T> pred) {
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return count(it, pred);
+    }
+
+    public static <T> int count(Iterable<T> iterable, Predicate<T> pred) {
+        final Iterator<T> it = iterable.iterator();
+        return count(it, pred);
+    }
+
+    public static <T> int count(Iterator<T> iterator, Predicate<T> pred) {
+        int result = 0;
+        while (iterator.hasNext()) {
+            T current = iterator.next();
+            if (pred.predicate(current)) {
+                result ++;
+            }
+        }
+        return result;
+    }
+
+    public static <T> T find(T[] array, T value) {
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return find(it, value);
+    }
+
+    public static <T> T find(Iterable<T> iterable, T value) {
+        final Iterator<T> it = iterable.iterator();
+        return find(it, value);
+    }
+
+    public static <T> T find(Iterator<T> iterator, T value) {
+        final Predicate<T> pred = value::equals;
+        return find(iterator, pred);
+    }
+
+    public static <T> T find(T[] array, Predicate<T> pred) {
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return find(it, pred);
+    }
+
+    public static <T> T find(Iterable<T> iterable, Predicate<T> pred) {
+        final Iterator<T> it = iterable.iterator();
+        return find(it, pred);
+    }
+
+    public static <T> T find(Iterator<T> iterator, Predicate<T> pred) {
+        while (iterator.hasNext()) {
+            T current = iterator.next();
+            if (pred.predicate(current)) {
+                return current;
+            }
+        }
+        return null;
     }
 }
